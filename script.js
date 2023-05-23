e7f76e28a45eeb4b886e63215dc65f0611e26794
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-05-23 16:29:08 +0200

    Reset failing

--- script.js
+++ script.js
@@ -5,32 +5,34 @@ const button = document.getElementById('button');
 //click on button to reset
 button.addEventListener('click', function () {
 
-
-
-// get base grid
-const gridElement = document. getElementById("grid");
-gridElement.classList.add("grid");
-
-// populate all 100 slots in grid and add effect on click
-for (let i = 1 ; i <= 100 ; i++){
-    const newCell = createElement("div", "cell");
-    let number = i
-    newCell.innerHTML = (number);
-
-    newCell.addEventListener ("click", function() {
-        newCell.classList.toggle("selected");
-        console.log(number);
-    }); 
+    let container = document.getElementById("container");
+    container.innerHTML = "";
     
-    gridElement.appendChild(newCell);
-}
+    // get base grid
+    const gridElement = document. getElementById("grid");
+    gridElement.classList.add("grid");
 
-//create cells
-function createElement(tagName, className){
-    const cellElement = document.createElement(tagName);
-    cellElement.className = className;
 
-    return cellElement;
-}
+    // populate all 100 slots in grid and add effect on click
+    for (let i = 1 ; i <= 100 ; i++){
+        const newCell = createElement("div", "cell");
+        let number = i
+        newCell.innerHTML = (number);
+
+        newCell.addEventListener ("click", function() {
+            newCell.classList.toggle("selected");
+            console.log(number);
+        }); 
+        
+        gridElement.appendChild(newCell);
+    }
+
+    //create cells
+    function createElement(tagName, className){
+        const cellElement = document.createElement(tagName);
+        cellElement.className = className;
+
+        return cellElement;
+    }
 
 });
